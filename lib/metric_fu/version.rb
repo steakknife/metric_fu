0c6f8b394d868bcf7e18db5de437575310548d28
Barry Allard <barry.allard@gmail.com>
2018-05-14 16:14:33 -0700

    bump version to 5.0.0pre1
    Signed-off-by: Barry Allard <barry.allard@gmail.com>

--- lib/metric_fu/version.rb
+++ lib/metric_fu/version.rb
@@ -1,9 +1,10 @@
 module MetricFu
   class Version
-    MAJOR = "4"
-    MINOR = "12"
+    MAJOR = "5"
+    MINOR = "0"
     PATCH = "0"
-    PRE   = ""
+    PRE   = "1"
   end
-  VERSION = [Version::MAJOR, Version::MINOR, Version::PATCH].join(".")
+  VERSION = [[Version::MAJOR, Version::MINOR, Version::PATCH].join("."),
+             !Version::PRE.empty? ?  "pre#{Version::PRE}" : nil].join
 end
